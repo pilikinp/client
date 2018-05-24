3ea33236494b722171fcb8df225674738166d290
Pegorino82 <eshkryabin@yandex.ru>
2018-05-24 15:14:58 +0300

    добавил комментарии к base/utils.py act_time

--- base/utils.py
+++ base/utils.py
@@ -1,9 +1,9 @@
 import time
 
-time_format = '%d.%m.%Y - %H:%M:%S'
 
-
-def act_time(time_):
+def act_time(time_, time_format='%d.%m.%Y - %H:%M:%S'):
+    '''преобразовывает время из секунд (с 01/01/1970 по настоящее время)
+    в соответствии с time_format'''
     t_local = time.localtime(time_)
     t = time.strftime(time_format, t_local)
     return t
