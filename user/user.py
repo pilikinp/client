c04a2c5937ad7a382d0e05625ce08dbb92aef791
Pegorino82 <eshkryabin@yandex.ru>
2018-05-24 15:17:33 +0300

    Добавил класс User

---
+++ user/user.py
@@ -0,0 +1,32 @@
+from base.base import FieldType
+
+
+class User:
+    '''
+    основные поля name и password, вся дополнительная информация в info в виде словаря
+    '''
+
+    name = FieldType('name', '', str)
+    password = FieldType('password', '', str)
+    info = FieldType('info', dict(), dict)
+
+    __slots__ = {name.name, password.name, info.name}
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @property
+    def user_dict(self):
+        _dict = dict()
+        for key in self.__slots__:
+            try:
+                value = getattr(self, key)
+                _dict[key] = value
+            except AttributeError:
+                pass
+        return _dict
+
+
+if __name__ == '__main__':
+    pass
