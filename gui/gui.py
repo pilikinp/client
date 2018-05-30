415c9cc4414063c9195a2df1cddc8c7d7972513c
glygs@mail.ru <glygs@mail.ru>
2018-05-30 17:10:54 +0500

    Merge remote-tracking branch 'origin/master'

--- gui/gui.py
+++ gui/gui.py
@@ -29,8 +29,8 @@ class MyWindow(QtWidgets.QMainWindow):
         self.monitor.moveToThread(self.thread)
         self.thread.started.connect(self.monitor.recv_msg)
         self.thread.start()
-        connect = self.monitor.client.run()
-        if not connect:
+        connect = self.monitor.client.socket._closed
+        if connect:
             QtWidgets.QMessageBox.warning(dialog, 'Warning!', 'Not connect')
             sys.exit()
 
